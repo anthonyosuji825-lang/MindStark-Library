0d978b7d04a139cea2586e1c221e862d9d1b876b
anthonyosuji825-lang <anthonyosuji825@gmail.com>
2026-04-27 12:30:49 +0100

    Add files via upload

--- archer-bubble.js
+++ archer-bubble.js
@@ -387,7 +387,14 @@
       const res = await fetch(SUPABASE_FUNCTION_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ messages: history }),
+        body: JSON.stringify({
+          messages: history,
+          currentDateTime: new Date().toLocaleString("en-NG", {
+            weekday: "long", year: "numeric", month: "long", day: "numeric",
+            hour: "2-digit", minute: "2-digit", second: "2-digit",
+            timeZoneName: "short"
+          })
+        }),
       });
       const data = await res.json();
       typingEl.remove();
